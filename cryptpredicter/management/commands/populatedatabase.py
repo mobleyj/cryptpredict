2631e20cfd56b35f739c115d8aa706edd9b95f0f
mobleyj <mob08001@byui.edu>
2018-04-04 20:49:28 -0600

    update to management to pull larger data pool

--- cryptpredicter/management/commands/populatedatabase.py
+++ cryptpredicter/management/commands/populatedatabase.py
@@ -5,13 +5,13 @@ from cryptpredicter.models import BpiPriceUpdate
 
 
 def populate_database():
-    url = 'https://api.coindesk.com/v1/bpi/historical/close.json'
+    url = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-01-01&end=2018-03-01'
 
     response = requests.get(url)
     data = response.json()
     i = 0
     for k, v in data['bpi'].items():
-        row = BpiPriceUpdate.objects.get_or_create(update_time=k, usd_value=v)
+        row = BpiPriceUpdate(update_time=k, usd_value=v)
         row.save()
         i += 1
     yield (i)
@@ -21,4 +21,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for i in populate_database():
-            print(i + ' rows created')
+            print(str(i) + ' rows created')
